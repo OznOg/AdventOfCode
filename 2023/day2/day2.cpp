817efd1847228271af3d8df5a5dbd23626fd180b
Sébastien Gonzalve <sebastien.github@gonzalve.net>
2023-12-05 20:28:55 +0100

    day2 part1

---
+++ 2023/day2/day2.cpp
@@ -0,0 +1,106 @@
+
+#include <iostream>
+#include <map>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using Set = std::vector<std::pair<unsigned, std::string>>;
+using Game = std::vector<Set>;
+using Data = std::map<unsigned, Game>;
+
+Set parse_set(std::string input) {
+    auto set = Set{};
+    do { 
+        auto pos = input.find(',');
+        auto str = input.substr(0, pos);
+
+        std::stringstream ss(str);
+
+        unsigned count = 0;
+        std::string color;
+        ss >> count >> color;
+
+        set.emplace_back(count, color);
+
+        if (pos != std::string::npos)
+            input = input.substr(pos + 1);
+        else
+            input.clear();
+    } while (not input.empty());
+    return set;
+};
+
+void handle_input(std::vector<Set> &set, std::string input) {
+    auto data = Data{};
+
+    do { 
+        auto pos = input.find(';');
+        auto str = input.substr(0, pos);
+
+        set.push_back(parse_set(str));
+
+        if (pos != std::string::npos)
+            input = input.substr(pos + 1);
+        else
+            input.clear();
+    } while (not input.empty());
+}
+
+std::map<std::string, unsigned> ref = {std::make_pair<std::string, unsigned>("red", 12), { "green", 13}, { "blue", 14 } };
+int main() {
+    std::string input;
+
+
+    auto data = Data{};
+
+    auto id = 1;
+    while (getline(std::cin, input)) {
+
+        input = input.substr(input.find(':') + 1);
+        
+        handle_input(data[id], input);
+        
+        id++;
+    }
+
+    //for (const auto &[i, vg] : data) {
+    //    std::cout << "{";
+    //    for (const auto &g : vg) {
+    //        std::cout << "  {";
+
+    //        for (auto &[n, c] : g) {
+    //            std::cout << ' ' << n << ' ' << c;
+    //        }
+    //        std::cout << " }\n";
+    //    }
+    //    std::cout << "}\n";
+    //}
+
+    auto isSetPossible = [&] (const Set &set) {
+        for (auto &[n, c] : set) {
+            if (n > ref[c])
+                return false;
+        }
+        return true;
+    };
+
+    auto isGamePossible = [&] (const Game &game) {
+        for (const auto &s : game) {
+           if (not isSetPossible(s))
+             return false;
+        }
+        return true;
+    };
+
+    unsigned sumOfIds = 0;
+    for (const auto &[i, g] : data) {
+       if (isGamePossible(g)) {
+          sumOfIds += i;       
+       }
+    }
+
+    std::cout << "Sum of ids is: " << sumOfIds << '\n';
+
+}
+
