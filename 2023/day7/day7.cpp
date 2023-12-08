ed0a560caec8148a04ebcf29573b414a0cfb208c
Sébastien Gonzalve <sebastien.github@gonzalve.net>
2023-12-08 19:07:20 +0100

    day7 part 2

--- 2023/day7/day7.cpp
+++ 2023/day7/day7.cpp
@@ -12,7 +12,7 @@ const auto cardstrength = std::map<char, unsigned>{
     std::make_pair<char, unsigned>('A', 14),
     { 'K', 13 },
     { 'Q', 12 },
-    { 'J', 11 },
+    { 'J', 1 },
     { 'T', 10 },
     { '9', 9 },
     { '8', 8 },
@@ -31,12 +31,44 @@ using Data = std::vector<std::tuple<Hand, unsigned, std::string>>;
 
 unsigned scoreHand(const Hand &hand) {
   if (hand.size() == 1) return 7; // Five of a kind
-  if (hand.size() == 2 && hand[0].second == 4) return 6; // Four of a kind
-  if (hand.size() == 2 && hand[0].second == 3) return 5; // Full house
-  if (hand.size() == 3 && hand[0].second == 3) return 4; // Three of a kind
-  if (hand.size() == 3 && hand[0].second == 2 && hand[1].second == 2) return 3; // Two pair
-  if (hand.size() == 4 && hand[0].second == 2) return 2; // One pair
-  if (hand.size() == 5) return 1; // High card
+  if (hand.size() == 2 && hand[0].second == 4) { // Four of a kind
+      if (hand[0].first == 'J' || hand[1].first == 'J')
+          return 7; // upgrate to Five of a kind
+      else
+          return 6;
+  }
+  if (hand.size() == 2 && hand[0].second == 3) { // Full house
+      if (hand[0].first == 'J' || hand[1].first == 'J')
+          return 7; // upgrate to Five of a kind
+      else
+          return 5;
+  }
+  if (hand.size() == 3 && hand[0].second == 3) { // Three of a kind
+      if (hand[0].first == 'J' || hand[1].first == 'J' || hand[2].first == 'J')
+          return 6; // upgrate to Four of a kind
+      else
+          return 4;
+  }
+  if (hand.size() == 3 && hand[0].second == 2 && hand[1].second == 2) { // two pairs
+      if (hand[0].first == 'J' || hand[1].first == 'J')
+          return 6; // upgrate to Four of a kind
+      else if (hand[2].first == 'J')
+          return 5; // upgrate to Full house
+      else
+          return 3; // Two pair
+  }
+  if (hand.size() == 4 && hand[0].second == 2) { // One pair
+      if (hand[0].first == 'J' || hand[1].first == 'J' || hand[2].first == 'J' || hand[3].first == 'J')
+          return 4; // upgrate to Three of a kind
+      else
+          return 2;
+  }
+  if (hand.size() == 5) { // High card
+      if (hand[0].first == 'J' || hand[1].first == 'J' || hand[2].first == 'J' || hand[3].first == 'J' || hand[4].first == 'J')
+          return 2; // upgrate to One pair
+      else
+          return 1;
+  }
   throw "WTF";
 }
 
