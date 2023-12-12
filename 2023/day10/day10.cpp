bc45a246953fb073e0df1c1db1b3d79234e1063a
Sébastien Gonzalve <sebastien.github@gonzalve.net>
2023-12-12 20:38:32 +0100

    day10 part 2

--- 2023/day10/day10.cpp
+++ 2023/day10/day10.cpp
@@ -76,12 +76,12 @@ int main() {
     bool first = true;
     while (getline(std::cin, input)) {
         if (first) {
-            data.emplace_back(std::string('.', input.size() + 2));
+            data.emplace_back(std::string(input.size() + 2, '.'));
             first = false;
         }
        data.emplace_back("." + input + ".");
     }
-    data.emplace_back(std::string('.', data.back().size()));
+    data.emplace_back(std::string(data.back().size(), '.'));
 
    std::cout << data.size() << '\n';
 
@@ -96,19 +96,118 @@ int main() {
       }
    }
 
+   auto path = std::vector<std::pair<unsigned, unsigned>>{};
+   path.push_back(origin);
    auto prev = origin;
    auto curr = find_start_first_pipe(data, {x, y});
+   path.push_back(curr);
 
    unsigned count = 1;
    while (curr != std::make_pair(x, y)) {
-       std::cout << "curr is (x:" << curr.first - 1 << ", y:" << curr.second  - 1 << ")\n";
+       //std::cout << "curr is (x:" << curr.first - 1 << ", y:" << curr.second  - 1 << ")\n";
        auto old_curr = curr;
        curr = next(data, prev, curr);
        prev = old_curr;
+       path.push_back(curr);
        count++;
    }
 
    std::cout << "Size of loop is: " << count << " farther place is: " << static_cast<unsigned>(ceil(count/2.)) << '\n';
+
+   Data data_clean;
+
+   for (auto j = unsigned{0}; j < data.size(); j++) {
+       auto l = data[j];
+       for (auto i = unsigned{0}; i < l.size(); i++) {
+           auto it = find(path.begin(), path.end(), std::make_pair(i, j));
+           if (it == path.end()) {
+               // not part of path, cleanu the value
+               l[i] = '.';
+           }
+           if (l[i] == 'S') { // FIX S
+               std::set<unsigned char> s;
+               s.insert('J');
+               s.insert('L');
+               s.insert('F');
+               s.insert('7');
+               s.insert('|');
+               s.insert('-');
+               if (l[i + 1] == 'J' || l[i + 1] == '-' || l[i + 1] == '7') {
+                  s.erase('|');
+                  s.erase('7');
+                  s.erase('J');
+               }
+               if (l[i - 1] == 'F' || l[i - 1] == 'L' || l[i - 1] == '-') {
+                  s.erase('|');
+                  s.erase('F');
+                  s.erase('L');
+               }
+               if (data[j + 1][i] == 'J' || data[j + 1][i] == 'L' || data[j + 1][i] == '|') {
+                  s.erase('-');
+                  s.erase('J');
+                  s.erase('L');
+               }
+               if (data[j - 1][i] == '7' || data[j - 1][i] == 'F' || data[j - 1][i] == '|') {
+                  s.erase('-');
+                  s.erase('7');
+                  s.erase('F');
+               }
+               l[i] = *s.begin();
+               std::cout << "Fixed S with:" << l[i] << '\n';
+           }
+       }
+       data_clean.push_back(l);
+   }
+
+   bool in = false;;
+   for (auto j = unsigned{0}; j < data_clean.size(); j++) {
+       auto &l = data_clean[j];
+       for (auto i = unsigned{0}; i < l.size(); i++) {
+          auto &c = l[i];
+          switch (c) {
+           case '|':
+             in = !in;
+             break;
+
+           case 'L':
+             for (i++; i < l.size() && l[i] == '-'; i++) {
+             }
+             if (l[i] == '7') in = !in;
+             break;
+           case 'F':
+             for (i++; i < l.size() && l[i] == '-'; i++) {
+             }
+             if (l[i] == 'J') in = !in;
+             break;
+           case 'J':
+           case '7':
+           case 'S':
+           case 'O':
+           case 'I':
+           break;
+           case '-':
+           break;
+           case '.':
+              l[i] = in ? 'I' : 'O';
+           break;
+           default:
+             throw "WTF";
+          }
+      }
+
+   }
+
+   
+   count = 0;
+   for (const auto &l : data_clean) {
+       std::cout << l << '\n';
+      for (const auto &c : l) {
+         if (c == 'I') count++;
+      }
+   }
+
+   std::cout << "Enclosed tiles count is: " << count << '\n';
+
 }
 
 
