ddf5286be3b34017e1a3c1c72aa61a3b9fe386e2
Sébastien Gonzalve <sebastien.github@gonzalve.net>
2023-12-13 21:53:10 +0100

    dy12 part2

--- 2023/day12/day12.cpp
+++ 2023/day12/day12.cpp
@@ -12,14 +12,17 @@
 using Springs = std::pair<std::string, std::vector<unsigned>>;
 using Data = std::vector<Springs>;
 
-unsigned count_matches(const std::string &l, std::vector<unsigned> &group) {
+unsigned matches(const std::string &l, std::vector<unsigned> &group) {
     auto g = std::vector<unsigned>{};
     g.push_back(0);
     unsigned idx = 0;
     while (idx < l.size()) {
         if (l[idx] == '.') {
             if (g.back() != 0)
-                g.push_back(0);
+               if (g.back() != group[g.size() - 1])
+                   return 0;
+               else
+                   g.push_back(0);
         } else {
             g.back()++;
         }
@@ -30,20 +33,36 @@ unsigned count_matches(const std::string &l, std::vector<unsigned> &group) {
     return g == group;
 }
 
-unsigned do_the_job(std::string l, std::vector<unsigned> &group) {
-  
-    auto pos = l.find('?');
-
-    if (pos == std::string::npos) {
-       // check matches
-       return count_matches(l, group);
+unsigned do_the_job(const std::string &l, std::vector<unsigned> &group, std::vector<unsigned> ref = {{0}}, size_t pos = 0) {
+   
+    if (pos == l.size()) {
+        if (ref.back() == 0)
+            ref.resize(ref.size() - 1);
+        return ref == group;
     }
-
-    l[pos] = '#';
-    auto c = do_the_job(l, group);
-    l[pos] = '.';
-
-    return c + do_the_job(l, group);
+    
+    if (l[pos] == '#') {
+        ref.back()++;
+        if (ref.back() > group[ref.size() - 1]) return 0;
+        return do_the_job(l, group, ref, pos + 1);
+    }
+    if (l[pos] == '.') {
+        if (ref.back() != 0) {
+            if (ref.back() != group[ref.size() - 1])
+                return 0;
+            else
+                ref.push_back(0);
+        }
+        return do_the_job(l, group, ref, pos + 1);
+    }
+    if (l[pos] == '?') {
+        auto l2 = l;
+        l2[pos] = '#';
+        auto c = do_the_job(l2, group, ref, pos);
+        l2[pos] = '.';
+        return c + do_the_job(l2, group, ref, pos);
+    }
+    throw "WTF";
 }
 
 int main() {
@@ -56,8 +75,6 @@ int main() {
        Springs spring;
        auto ss = std::stringstream{input};
        ss >> spring.first >> groups_str; 
-
-       spring.first = '.' + spring.first + '.'; // make sure some empty stuff is on side (eases search)
 
        while (not groups_str.empty()) {
           auto pos = groups_str.find(',');
@@ -73,8 +90,30 @@ int main() {
     }
 
    unsigned long sum = 0;
+   #if 0
    for (auto &[l, g] : data) {
        auto count = do_the_job(l, g);
+       sum += count;
+   }
+   #endif
+
+   std::cout << "Sum is: " << sum << '\n';
+
+   auto unfolded_data = data;
+   for (auto &[l, g] : unfolded_data) {
+       l = l + '?' + l + '?' + l + '?' + l + '?' + l;
+       auto g2 = g;
+       g2.insert(end(g2), begin(g), end(g));
+       g2.insert(end(g2), begin(g), end(g));
+       g2.insert(end(g2), begin(g), end(g));
+       g2.insert(end(g2), begin(g), end(g));
+       g = g2;
+   }
+
+   for (auto &[l, g] : unfolded_data) {
+       std::cout << "processing: " << l;
+       auto count = do_the_job(l, g);
+       std::cout << " " << count << '\n';
        sum += count;
    }
 
