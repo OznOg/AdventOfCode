3ada592dd2e44a0a0b7dd320dfa691251fbcc136
Sébastien Gonzalve <sebastien.github@gonzalve.net>
2024-12-05 21:13:23 +0100

    day5p2

--- 2024/day5/day5.cpp
+++ 2024/day5/day5.cpp
@@ -8,6 +8,7 @@
 #include <algorithm>
 #include <format>
 #include <iostream>
+#include <set>
 #include <sstream>
 #include <string>
 #include <vector>
@@ -25,13 +26,15 @@ bool update_match_rule(const Update &update, const unsigned& first, const std::v
      return true;
    }
 
+   auto res = true;
    for (const auto &e : list) {
       if (std::find(begin(update), it, e) != it) {
-        return false;
+       // fmt::print("{} fails rule {}|{}\n", update, first, e);
+        res = false;
       }
    }
 
-   return true;
+   return res;
 }
 
 bool update_match_rules(const Update &update, const Rules& rules) {
@@ -55,6 +58,58 @@ auto filter(const Updates &updates, const Rules& rules) {
   return filtered;
 }
 
+auto filter_bad(const Updates &updates, const Rules& rules) {
+  Updates filtered;
+
+  for (auto &u : updates) {
+    if (not update_match_rules(u, rules)) {
+      filtered.push_back(u);
+    }
+  }
+
+  return filtered;
+}
+
+
+bool is_after_another(unsigned e, const std::set<unsigned>& update, const Rules& rules) {
+  for (auto &[first, list] : rules) {
+    if (not update.contains(first)) continue;
+
+    for (auto v : list) {
+      if (v == e) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+auto fix_update(const Update &bad, const Rules& rules) {
+  auto b = std::set(bad.begin(), bad.end());
+
+  auto fixed = Update{};
+  while (not b.empty()) {
+    for (auto e : b) {
+      if (not is_after_another(e, b, rules)) {
+        fixed.push_back(e);
+        b.erase(e);
+        break;
+      }
+    }
+  }
+  return fixed;
+}
+
+auto fix_updates(const Updates &updates, const Rules& rules) {
+  Updates all_fixed;
+  
+  for (auto &update : updates) {
+    auto fixed = fix_update(update, rules);
+    all_fixed.push_back(fixed);
+  }
+
+  return all_fixed;
+}
 
 int main() {
 
@@ -80,6 +135,7 @@ int main() {
             updates.push_back(update);
     }
 
+{ // p1
     fmt::print("updates: {}\n", updates);
 
     auto filtered = filter(updates, rules);
@@ -90,5 +146,24 @@ int main() {
     }
 
     fmt::print("sum is {}\n", sum);
+    }
+    { //p2
+
+    auto bad = filter_bad(updates, rules);
+
+   fmt::print("bad is {}\n", bad);
+
+
+   auto fixed = fix_updates(bad, rules);
+   fmt::print("good is {}\n", fixed);
+    
+    auto sum = unsigned{};
+    for (auto &u : fixed) {
+      sum += u[(u.size() ) / 2];
+    }
+
+    fmt::print("sum is {}\n", sum);
+   }
+
 }
 
