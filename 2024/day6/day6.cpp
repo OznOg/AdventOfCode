27e2cc2d6377dd116f900f8e82343937f6e91254
Sébastien Gonzalve <sebastien.github@gonzalve.net>
2024-12-06 12:02:52 +0100

    day6p1

---
+++ 2024/day6/day6.cpp
@@ -0,0 +1,126 @@
+
+
+
+#include <fmt/format.h>
+#include <fmt/ranges.h>
+#include <fmt/std.h>
+#include <array>
+#include <algorithm>
+#include <format>
+#include <iostream>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <ranges>
+#include <regex>
+
+
+using Map = std::vector<std::string>;
+
+struct Guard {
+  enum class Direction { N, E, S, O };
+
+  Direction dir;
+
+  struct { 
+   int x, y;
+  } pos;
+};
+
+
+
+auto move(Guard &guard, const Map& map, Map &path) {
+  if (guard.pos.x >= map[0].size()
+      or guard.pos.x < 0
+      or guard.pos.y >=  map.size()
+      or guard.pos.y < 0) {
+    return false;
+  }
+
+  path[guard.pos.y][guard.pos.x] = 'X';
+ 
+  switch(guard.dir) {
+    case Guard::Direction::N:
+        if (guard.pos.y - 1 >= 0
+            and map[guard.pos.y - 1][guard.pos.x] == '#') {
+          guard.dir = Guard::Direction::E;
+        } else {
+          guard.pos.y--;
+        }
+        break;
+    case Guard::Direction::O:
+        if (guard.pos.x - 1 >= 0
+            and map[guard.pos.y][guard.pos.x - 1] == '#') {
+          guard.dir = Guard::Direction::N;
+        } else {
+          guard.pos.x--;
+        }
+        break;
+
+    case Guard::Direction::S:
+        if (guard.pos.y + 1 <= map.size() - 1
+            and map[guard.pos.y + 1][guard.pos.x] == '#') {
+          guard.dir = Guard::Direction::O;
+        } else {
+          guard.pos.y++;
+        }
+        break;
+    case Guard::Direction::E:
+        if (guard.pos.x + 1 <= map[0].size() - 1
+            and map[guard.pos.y][guard.pos.x + 1] == '#') {
+          guard.dir = Guard::Direction::S;
+        } else {
+          guard.pos.x++;
+        }
+        break;
+  }
+
+  return true;
+}
+
+int main() {
+   
+    auto map = Map{};
+    auto path = Map{};
+    auto guard = Guard{};
+    auto idx = 0u;
+    auto input = std::string{};
+    while (getline(std::cin, input)) {
+        auto it = std::ranges::find_if(input, [] (auto &c) { return c != '.' and c != '#'; });
+        if (it != input.end()) {
+           guard.pos.y = idx;
+           guard.pos.x = it - input.begin();
+
+           switch(*it) {
+             case '^': guard.dir = Guard::Direction::N; break;
+             case '>': guard.dir = Guard::Direction::E; break;
+             case 'v': guard.dir = Guard::Direction::S; break;
+             case '<': guard.dir = Guard::Direction::O; break;
+           }
+
+           *it = '.';
+        }
+        map.push_back(input);
+        path.emplace_back(input.size(), ' ');
+        idx++;
+    }
+
+
+    fmt::print("Map\n{}\n", fmt::join(map, "\n"));
+
+    while (move(guard, map, path)) {
+    //  fmt::print("Path\n{}\n", fmt::join(path, "\n"));
+      ;
+    }
+
+    fmt::print("Path\n{}\n", fmt::join(path, "\n"));
+    
+    auto count = 0u;
+    for (auto &c : fmt::format("{}", fmt::join(path, ""))) {
+      if (c == 'X') count++;
+    }
+
+    fmt::print("count is: {}\n", count);
+}
+
