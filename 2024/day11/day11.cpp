2f8940ac9a8788c53a816cb5ef83bec8be9b19d4
Sébastien Gonzalve <sebastien.github@gonzalve.net>
2024-12-11 08:52:39 +0100

    day11p1

--- 2024/day11/day11.cpp
+++ 2024/day11/day11.cpp
@@ -19,17 +19,29 @@
 
 using Stones = std::list<unsigned long long>;
 
-void blink(Stones &stone) {
+unsigned long long my_exp10(unsigned pow) {
+    unsigned long long res = 1;
+    for (auto i = 0; i < pow; i++) {
+       res *= 10;
+    }
+    return res;
+}
+
+void blink(Stones &stones) {
    for (auto it = stones.begin(); it != stones.end(); ++it) {
       if (*it == 0) {
         *it = 1;
-        return;
+        continue;
       }
       auto size = static_cast<long>(std::log10(*it) + 1);
       if ((size % 2) == 0) {
-          auto div = std::div(*it, );
-
+          auto div = std::lldiv(*it, my_exp10(size / 2));
+          stones.insert(it, div.quot);
+          *it = div.rem;
+          continue;
       }
+
+      *it *= 2024;
    }
 }
 
@@ -46,5 +58,10 @@ int main() {
   }
 
   fmt::print("Data is: {}\n", data);
+
+  for (auto i = 0; i < 25; i++) {
+    blink(data);
+  }
+  fmt::print("After 25 blink: {}\n", data.size());
 }
 
