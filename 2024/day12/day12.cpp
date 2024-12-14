ba80cb14986973ce6df36de831f9eadeb8a7cdc0
Sébastien Gonzalve <sebastien.github@gonzalve.net>
2024-12-14 13:53:45 +0100

    Much better day12

--- 2024/day12/day12.cpp
+++ 2024/day12/day12.cpp
@@ -79,58 +79,35 @@ auto  make_regions(const Map& _map) {
    return regions;
 }
 
-size_t count_vector(std::vector<int> v) {
-   size_t count = 0;
-
-   std::ranges::sort(v);
-   auto idx = -12;
-   for (const auto &e : v) {
-      if (e != idx) {
-         count++;
-         idx = e;
-      }
-      idx += 1;
-   }
-  return count;
-}
-
-auto compute_sides(const Region& r, const Map& map) {
-    std::map<int, std::map<int, std::vector<int>>> sides_up;
-    std::map<int, std::map<int, std::vector<int>>> sides_down;
-    std::map<int, std::map<int, std::vector<int>>> sides_left;
-    std::map<int, std::map<int, std::vector<int>>> sides_right;
+auto compute_angles(const Region& r, const Map& map) {
+    auto angle = 0u;
 
     for (auto g : r.gardens) {
-         if (map[g.y][g.x - 1] != r.plant) sides_left[g.x][g.x - 1].push_back(g.y);
-         if (map[g.y][g.x + 1] != r.plant) sides_right[g.x][g.x + 1].push_back(g.y);
-         if (map[g.y - 1][g.x] != r.plant) sides_up[g.y][g.y - 1].push_back(g.x);
-         if (map[g.y + 1][g.x] != r.plant) sides_down[g.y][g.y + 1].push_back(g.x);
-    }
-    
-    auto size = 0u;
-    for (auto &[i, m]: sides_up) {
-      for (auto [j, v] : m) {
-        size += count_vector(v);
-      }
-    }
-    for (auto &[i, m] : sides_down) {
-      for (auto [j, v] : m) {
-        size += count_vector(v);
-      }
-    }
-    for (auto &[i, m]: sides_left) {
-      for (auto [j, v] : m) {
-        size += count_vector(v);
-      }
-    }
-    for (auto &[i, m]: sides_right) {
-      for (auto [j, v] : m) {
-        size += count_vector(v);
-      }
-    }
-    return size;
-}
+         if (map[g.y][g.x - 1] != r.plant && map[g.y - 1][g.x] != r.plant) angle++; // |^
+         if (map[g.y][g.x + 1] != r.plant && map[g.y - 1][g.x] != r.plant) angle++; // ^|
+         if (map[g.y][g.x + 1] != r.plant && map[g.y + 1][g.x] != r.plant) angle++; // _|
+         if (map[g.y][g.x - 1] != r.plant && map[g.y + 1][g.x] != r.plant) angle++; // |_
 
+         if (map[g.y][g.x + 1] != r.plant
+             && map[g.y + 1][g.x] == r.plant
+             && map[g.y + 1][g.x + 1] == r.plant
+             ) angle++;
+         if (map[g.y][g.x - 1] != r.plant
+             && map[g.y + 1][g.x] == r.plant
+             && map[g.y + 1][g.x - 1] == r.plant
+             ) angle++;
+         if (map[g.y][g.x - 1] != r.plant
+             && map[g.y - 1][g.x] == r.plant
+             && map[g.y - 1][g.x - 1] == r.plant
+             ) angle++;
+         if (map[g.y][g.x + 1] != r.plant
+             && map[g.y - 1][g.x] == r.plant
+             && map[g.y - 1][g.x + 1] == r.plant
+             ) angle++;
+    }
+    return angle;
+}
+    
 int main() {
 
   auto map = Map{};
@@ -155,8 +132,8 @@ int main() {
   unsigned long long sum2 = 0;
   for (auto r : regions) {
      sum += r.gardens.size() * r.fence_length;
-     auto sides = compute_sides(r, map);
-     //fmt::print("Sides for {}: {}\n", std::string(1, r.plant), sides);
+     auto sides = compute_angles(r, map);
+     fmt::print("Sides for {}: {}\n", std::string(1, r.plant), sides);
      sum2 += sides * r.gardens.size();
   }
 
