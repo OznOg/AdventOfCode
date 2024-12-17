601f6c9a9b09151e64850980a4b04fdbf0e77e16
Sébastien Gonzalve <sebastien.github@gonzalve.net>
2024-12-17 12:31:44 +0100

    day17p1

---
+++ 2024/day17/day17.cpp
@@ -0,0 +1,152 @@
+
+
+#include <fmt/format.h>
+#include <fmt/ranges.h>
+#include <fmt/std.h>
+#include <array>
+#include <algorithm>
+#include <cstdlib>
+#include <cmath>
+#include <format>
+#include <iostream>
+#include <optional>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <list>
+#include <ranges>
+#include <regex>
+
+
+auto exp2(unsigned x) {
+  unsigned v = 1;
+  while (x) {
+     v *= 2;
+     x--;
+  }
+  return v;
+}
+
+struct Computer {
+  long long A = 0, B = 0, C = 0;
+
+  unsigned ip = 0;
+
+  std::vector<unsigned> output;
+  struct Instr {
+      unsigned code;
+      unsigned op;
+      friend auto format_as(const Instr &i) {
+        return fmt::format("(code={}, op={})", i.code, i.op);
+      }
+  };
+  std::vector<Instr> prog;
+
+  friend auto format_as(const Computer &c) {
+    return fmt::format("(A={}, B={}, C={}, ip={}, prog={})", c.A, c.B, c.C, c.ip, c.prog);
+  }
+
+  unsigned op2val(const unsigned &op) {
+      switch (op) {
+          case 0:
+          case 1:
+          case 2:
+          case 3:
+              return op;
+          case 4:
+              return A;
+          case 5:
+              return B;
+          case 6:
+              return C;
+      }
+      throw "WTF";
+  }
+
+  void run() {
+    while (run_one())
+      ;
+  }
+  bool run_one() {
+     if (ip >= prog.size()) return false;
+
+     auto instr = prog.at(ip);
+     switch (instr.code) {
+         case 0: //adv
+             A /= exp2(op2val(instr.op));
+             ip++;
+             break;
+         case  1: // bxl
+             B ^= instr.op;
+             ip++;
+             break;
+         case 2: // bst
+             B = op2val(instr.op) % 8;
+             ip++;
+             break;
+         case 3: // jnz
+             if (A != 0) {
+                 if (instr.op % 2) throw "WTF";
+                 ip = instr.op / 2;
+             } else ip++;
+             break;
+
+         case 4: //bxc
+             B ^= C;
+             ip++;
+             break;
+         case 5: //out
+             output.push_back(op2val(instr.op) % 8);
+             ip++;
+             break;
+
+         case 6: // bdv
+             B = A / exp2(op2val(instr.op));
+             ip++;
+             break;
+
+         case 7: // cdv
+             C = A / exp2(op2val(instr.op));
+             ip++;
+             break;
+         default:
+             throw "WTF";
+     }
+     return true;
+  }
+};
+
+int main() {
+
+  auto computer = Computer{};
+  auto input = std::string{};
+  std::regex r(R"((\d+))");
+  std::smatch sm;
+  getline(std::cin, input);
+  regex_search(input, sm, r);
+  computer.A = stol(sm[1].str());
+  getline(std::cin, input);
+  regex_search(input, sm, r);
+  computer.B = stol(sm[1].str());
+  getline(std::cin, input);
+  regex_search(input, sm, r);
+  computer.C = stol(sm[1].str());
+  getline(std::cin, input); // empty line
+  getline(std::cin, input);
+  for (; regex_search(input, sm, r);) {
+    auto code = stol(sm[1].str());
+    input = sm.suffix();
+    regex_search(input, sm, r);
+    auto op = stol(sm[1].str());
+    computer.prog.emplace_back(code, op);
+    input = sm.suffix();
+  }
+
+  fmt::print("Computer is:\n{}\n", computer);
+  computer.run();
+  fmt::print("Computer is:\n{}\n", computer);
+  fmt::print("Output is:\n{}\n", fmt::join(computer.output, ","));
+}
+
+
